b4ec2a82a263267886466af338149f237c8c49ce
Diego Vasquez <botellongo69@outlook.com>
2020-02-19 18:24:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,7 @@ class Student
   end
   
   def self.create_table
-    sql = "CREATE TABLE IF NOT EXISTS students"
+    sql = "CREATE TABLE IF NOT EXISTS students (id PRIMARY KEY)"
   end
   
   def self.drop_table
